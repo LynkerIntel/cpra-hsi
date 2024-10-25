55b8cd5683e9bf45d68a42d558978385cf476b85
Dillon Ragar <dillonragar@gmail.com>
2024-10-25 16:24:40 -0600

    add salinity rough draft

--- VegProcessor/veg_logic.py
+++ VegProcessor/veg_logic.py
@@ -1,13 +1,21 @@
-def zone_v(arg1, arg2, arg3):
+import numpy as np
+
+
+def zone_v(arg1, arg2, arg3) -> np.ndarray:
     """ """
     return NotImplementedError
 
 
-def zone_iv(arg1, arg2, arg3):
+def zone_iv(arg1, arg2, arg3) -> np.ndarray:
     """ """
     return NotImplementedError
 
 
-def zone_iii(arg1, arg2, arg3):
+def zone_iii(arg1, arg2, arg3) -> np.ndarray:
     """ """
+    return NotImplementedError
+
+
+def habitat_based_salinity() -> np.ndarray:
+    """Get salinity defaults based on habitat type."""
     return NotImplementedError
--- VegProcessor/veg_transition.py
+++ VegProcessor/veg_transition.py
@@ -7,7 +7,8 @@ import veg_logic
 
 
 class VegTransition:
-    """The Vegetation Transition Model."""
+    """The Vegetation Transition Model.
+    """
     def __init__(self, config_file, log_level=logging.INFO):
         """
         Initialize by setting up logger, loading resource paths, and creating empty
@@ -19,6 +20,7 @@ class VegTransition:
         self.pct_mast_hard
         self.pct_mast_soft
         self.pct_no_mast
+        self.salinity
 
         Parameters:
         - config_file (str): Path to configuration YAML
@@ -30,6 +32,7 @@ class VegTransition:
         # fetch raster data paths
         self.dem_path = config["raster_data"].get("dem_path")
         self.veg_base_path = config["raster_data"].get("veg_base_raster")
+        self.salinity_path = config['raster_data'].get("salinity_raster")
 
         # Extract initial state variables (dummy vars) from config
         self.alpha = config["parameters"].get("alpha", 0.1)
@@ -61,17 +64,22 @@ class VegTransition:
         # Load veg base and use as template to create arrays for the main state variables
         self.load_veg_base_raster()
 
+
         # create empty arrays for state variables, based on x, y dims of veg type base raster
         template = np.zeros((self.veg_type.ny, self.veg_type.nx))
 
         self.dem # = load dem
         self.wse # = load wse
+        
+        # load raster if provided, default values if not
+        self.load_salinity()
 
         self.veg_type = template
         self.maturity = template
         self.pct_mast_hard = template
         self.pct_mast_soft = template
         self.pct_no_mast = template
+        
 
     def _setup_logger(self, log_level):
         """Set up the logger for the class."""
@@ -93,7 +101,7 @@ class VegTransition:
             # Add the handler to the logger
             self._logger.addHandler(ch)
 
-    def step(self, dt=0.01):
+    def step(self, dt=1):
         """Advance the transition model by one step."""
 
         # example of how to call veg logic module
@@ -111,7 +119,7 @@ class VegTransition:
 
 
         # TODO: update for actual timestep, assuming year now
-        self.maturity += 1
+        self.maturity += dt
 
         # Update time
         self.time += dt
@@ -169,3 +177,26 @@ class VegTransition:
         """
         ds = xr.open_dataset(self.veg_base_path)
         self.veg_type = ds["veg"]
+
+    def load_salinity(self) -> np.ndarray:
+        """Load salinity raster data (if available.)
+        """
+        #raise NotImplementedError
+    
+        # salinity
+        # From Jenneke:
+
+        # If 60m pixel is saline marsh then set salinity to 18;
+        # Else if pixel is brackish marsh then set salinity to 8;
+        # Else if pixel is intermediate marsh then set salinity to 3.5;
+        # Else set salinity to 1.
+    
+        if self.salinity_path:
+            self.salinity = # load
+            self._logger.info('Loaded salinity from raster')
+        else:
+            self.salinity = veg_logic.habitat_based_salinity(
+                self.veg_type
+            )
+
+            self._logger.info('Creating salinity from habitat defaults')

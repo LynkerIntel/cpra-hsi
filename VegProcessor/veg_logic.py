7f861cbd075b11a5d48085f291f71a08e70d841b
Dillon Ragar <dillonragar@gmail.com>
2024-10-30 17:14:13 -0700

    refactor to mutable veg_type array

--- VegProcessor/veg_logic.py
+++ VegProcessor/veg_logic.py
@@ -1,9 +1,45 @@
 import numpy as np
+import xarray as xr
 
 
-def zone_v(arg1, arg2, arg3) -> np.ndarray:
-    """ """
-    return NotImplementedError
+def zone_v(veg_type: np.ndarray, depth: xr.Dataset) -> np.ndarray:
+    """Calculate transition for Zone V
+
+    MAR, APRIL, MAY, or JUNE
+    inundation depth <= 0, AND
+    GS inundation >20%
+
+    Params:
+        - zone_v (np.ndarray): array of current zone_v locations, with np.nan elsewhere
+        - inundation (xr.Dataset): Dataset of 1 year of inundation depth from hydrologic model,
+            created from water surface elevation and the domain DEM.
+
+    Returns:
+        - arr (np.ndarray): Output array of new values contining valid transitions,
+            so this output array contains are mix of old zone v pixels and new values.
+    """
+    #return NotImplementedError
+
+    # initialize return array with no changes
+    veg_out = veg_type.copy()
+
+    # subset to veg type v
+    veg_v = veg_type[veg_type == int]
+
+    # # create mask for depth condition
+    mask_depth = depth.sel(slice=mar to june) <= 0
+
+    # # create mask for temporal condition
+    # inundation_gs = depth.sel(slice=growing season)
+    # inundation_gs[inundation_gs != 0]
+
+    # # create mask where both conditions are true
+    # change_mask = # &
+
+    # if change:
+    #     arr[mask] = habitat int
+
+    # return arr
 
 
 def zone_iv(arg1, arg2, arg3) -> np.ndarray:
@@ -14,23 +50,3 @@ def zone_iv(arg1, arg2, arg3) -> np.ndarray:
 def zone_iii(arg1, arg2, arg3) -> np.ndarray:
     """ """
     return NotImplementedError
-
-
-def habitat_based_salinity() -> np.ndarray:
-    """Get salinity defaults based on habitat type.
-
-
-    From Jenneke:
-
-    If 60m pixel is saline marsh then set salinity to 18;
-    Else if pixel is brackish marsh then set salinity to 8;
-    Else if pixel is intermediate marsh then set salinity to 3.5;
-    Else set salinity to 1.
-    """
-    return NotImplementedError
-
-    # condition 1
-    self.veg_type["saline march"] == 18
-
-    # mask_1 = self.veg_type == "saline marsh"
-    # si_1[mask_1] = ((4.5 * self.v1_pct_open_water[mask_1]) / 100) + 0.1
--- VegProcessor/veg_transition.py
+++ VegProcessor/veg_transition.py
@@ -2,20 +2,27 @@ import logging
 import yaml
 import xarray as xr
 import numpy as np
+import geopandas as gpd
+import pandas as pd
 
 import veg_logic
+import hydro_logic
 
 
 class VegTransition:
     """The Vegetation Transition Model.
+
+    Vegetation zones are calculated independenlty (slower) for
+    better model interperability and quality control.
     """
+
     def __init__(self, config_file, log_level=logging.INFO):
         """
         Initialize by setting up logger, loading resource paths, and creating empty
         arrays for state variables. State variables are:
 
         self.veg_type
-        self.maturity 
+        self.maturity
         self.elevation
         self.pct_mast_hard
         self.pct_mast_soft
@@ -32,7 +39,8 @@ class VegTransition:
         # fetch raster data paths
         self.dem_path = config["raster_data"].get("dem_path")
         self.veg_base_path = config["raster_data"].get("veg_base_raster")
-        self.salinity_path = config['raster_data'].get("salinity_raster")
+        self.veg_keys_path = config["raster_data"].get("veg_keys")
+        self.salinity_path = config["raster_data"].get("salinity_raster")
 
         # Extract initial state variables (dummy vars) from config
         self.alpha = config["parameters"].get("alpha", 0.1)
@@ -46,40 +54,42 @@ class VegTransition:
         self.time = 0
 
         # Store history for analysis
-        self.history = {"P": [self.P], "H": [self.H], "time": [self.time]}
-
+        # self.history = {"P": [self.P], "H": [self.H], "time": [self.time]}
 
         # Set up the logger
         self._setup_logger(log_level)
 
-        # Log the initialization using lazy formatting
-        self._logger.info(
-            "Initialized model with P0=%s, H0=%s, alpha=%s, beta=%s,
-            self.dummy_var,
-            self.dummy_var,
-            self.dummy_var,
-            self.dummy_var,
-        )
+        # # Log the initialization using lazy formatting
+        # self._logger.info(
+        #     "Initialized model with P0=%s, H0=%s, alpha=%s, beta=%s,
+        #     self.dummy_var,
+        #     self.dummy_var,
+        #     self.dummy_var,
+        #     self.dummy_var,
+        # )
 
         # Load veg base and use as template to create arrays for the main state variables
-        self.load_veg_base_raster()
+        self.veg_type = self.load_veg_initial_raster()
+        self.veg_keys = self.load_veg_keys()
 
+        # self.zone_v = self.veg_type[self.veg_type == 23]
 
         # create empty arrays for state variables, based on x, y dims of veg type base raster
-        template = np.zeros((self.veg_type.ny, self.veg_type.nx))
+        # template = np.zeros((self.veg_type.ny, self.veg_type.nx))
 
-        self.dem # = load dem
-        self.wse # = load wse
-        
+        self.dem  # = load dem
+        self.wse  # = load wse
+        # caluculate depth
+        self.depth = self.get_depth(self.dem, self.wse)
+
         # load raster if provided, default values if not
-        self.load_salinity()
+        # self.load_salinity()
 
-        self.veg_type = template
-        self.maturity = template
-        self.pct_mast_hard = template
-        self.pct_mast_soft = template
-        self.pct_no_mast = template
-        
+        # self.veg_type = template
+        # self.maturity = template
+        # self.pct_mast_hard = template
+        # self.pct_mast_soft = template
+        # self.pct_no_mast = template
 
     def _setup_logger(self, log_level):
         """Set up the logger for the class."""
@@ -103,20 +113,23 @@ class VegTransition:
 
     def step(self, dt=1):
         """Advance the transition model by one step."""
+        # calculate inundation
 
         # example of how to call veg logic module
-        self.zone_v = veg_logic.zone_v(
-            self.dem,
-            self.wse,
-        )
+        self.veg_type = veg_logic.zone_v(self.veg_type, self.depth)
+        self.veg_type = veg_logic.zone_iv(self.veg_type, self.depth)
 
-        # repeat for all necessary state vars
-        self.zone_iv = veg_logic.zone_iv(
-            self.dem,
-            self.wse
-        )
+        self.new_veg_3
 
+        # combine all zones into new timestep
+        # must first ensure that there are no overlapping
+        # values. Need a good QC method here.
+        self.veg_type = self.new_veg_1 + self.new_veg_2
 
+        self.create_new_veg_arrays()
+
+        # if veg type has changed maturity = 0,
+        # if veg type has not changes, maturity + 1
 
         # TODO: update for actual timestep, assuming year now
         self.maturity += dt
@@ -129,7 +142,6 @@ class VegTransition:
         self.history["H"].append(self.H)
         self.history["time"].append(self.time)
 
-        # Log the current state using lazy formatting
         self._logger.debug(
             "Time: %.2f, var1: %.2f, var2: %.2f",
             self.time,
@@ -159,35 +171,38 @@ class VegTransition:
         self._logger.info("Simulation complete")
 
     def load_dem(self) -> np.ndarray:
-        """Load project domain DEM. 
-        """
+        """Load project domain DEM."""
         # self.dem = load
         raise NotImplementedError
 
     def load_landcover(self) -> np.ndarray:
         """This method will load the landcover dataset, which may
-        be needed? 
+        be needed?
         """
         raise NotImplementedError
-    
-    def load_veg_base(self) -> np.ndaray:
+
+    def load_veg_initial_raster(self):
         """This method will load the base veg raster, from which the model will iterate forwards,
         according to the transition logic.
 
         """
-        ds = xr.open_dataset(self.veg_base_path)
-        self.veg_type = ds["veg"]
+        da = xr.open_dataarray(self.veg_base_path)
+        return da
 
-    def load_salinity(self) -> np.ndarray:
-        """Load salinity raster data (if available.)
-        """
-        #raise NotImplementedError
+    def load_veg_keys(self) -> pd.DataFrame:
+        """load vegetation class names from database file"""
+        dbf = gpd.read_file(self.veg_keys_path)
+        # fix dtype
+        dbf["Value"] = dbf["Value"].astype(int)
+        return dbf
+
+    def load_salinity(self):
+        """Load salinity raster data (if available.)"""
+        # raise NotImplementedError
         if self.salinity_path:
-            self.salinity = # load
-            self._logger.info('Loaded salinity from raster')
+            self.salinity = None
+            self._logger.info("Loaded salinity from raster")
         else:
-            self.salinity = veg_logic.habitat_based_salinity(
-                self.veg_type
-            )
+            self.salinity = hydro_logic.habitat_based_salinity(self.veg_type)
 
-            self._logger.info('Creating salinity from habitat defaults')
+            self._logger.info("Creating salinity from habitat defaults")

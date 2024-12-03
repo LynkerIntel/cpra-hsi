66cc7971febf33c8a5589e0a97e16c7d3a7454db
Dillon Ragar <dillonragar@gmail.com>
2024-12-03 15:44:31 -0800

    rm `qc_output` need to reimplement better at some point

--- VegProcessor/testing.py
+++ VegProcessor/testing.py
@@ -1,29 +1,34 @@
 import numpy as np
 import functools
+import logging
 from typing import Callable, List, Tuple
 
 
-def qc_output(func: Callable) -> Callable:
-    """
-    Decorator to check if the output array differs from the input array.
-    Logs a message if no changes are detected.
-    """
+# # Configure the logger in VegTransition
+# logger = logging.getLogger("VegTransition")
 
-    @functools.wraps(func)
-    def wrapper(*args: np.ndarray, **kwargs) -> np.ndarray:
-        # Expecting the veg_type array to be the second argument (args[1])
-        veg_type_input = args[1].copy()  # Copy the input array to compare later
 
-        # Run the wrapped function
-        result = func(*args, **kwargs)
+# def qc_output(func: Callable) -> Callable:
+#     """
+#     Decorator to check if the output array differs from the input array.
+#     Logs a message if no changes are detected.
+#     """
 
-        # Compare the result with the input
-        if np.array_equal(veg_type_input, result):
-            logger = args[0]  # Assuming the first argument is the logger
-            logger.info(f"No changes made to vegetation pixels in {func.__name__}.")
-        return result
+#     @functools.wraps(func)
+#     def wrapper(*args: np.ndarray, **kwargs) -> np.ndarray:
+#         # Expecting the veg_type array to be the second argument (args[1])
+#         veg_type_input = args[0].copy()  # Copy the input array to compare later
 
-    return wrapper
+#         # Run the wrapped function
+#         result = func(*args, **kwargs)
+
+#         # Compare the result with the input
+#         if np.array_equal(veg_type_input, result):
+#             # logger = args[0]  # Assuming the first argument is the logger
+#             logger.info(f"No changes made to vegetation pixels in {func.__name__}.")
+#         return result
+
+#     return wrapper
 
 
 # def check_mask_overlap(masks: List[np.ndarray]) -> None:

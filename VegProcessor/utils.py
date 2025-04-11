8d1a3414f9b6fe45e727ee118163789be22f2161
Dillon Ragar <dillonragar@gmail.com>
2025-04-11 16:14:19 -0700

    load sequenes into single ts dict

--- VegProcessor/utils.py
+++ VegProcessor/utils.py
@@ -801,6 +801,33 @@ def dataset_attrs_to_df(ds: xr.Dataset, selected_attrs: list) -> pd.DataFrame:
     return pd.DataFrame(rows)
 
 
+def load_sequence_csvs(directory: str) -> dict[int, int]:
+    """load all csvs in a directory that include 'sequence' in the name
+
+    keys in the output dict are the first part of each filename (before the first underscore)
+
+    :param (str) directory: path to the directory containing sequence csvs
+    :return (dict[int, int]): dictionary of "Year" and "Quintile" as ints
+    """
+    csv_dict = {}
+    for file in os.listdir(directory):
+        if file.endswith(".csv") and "sequence" in file:
+            prefix = file.split("_")[0]
+            file_path = os.path.join(directory, file)
+            csv_dict[prefix] = pd.read_csv(file_path, header=1)
+            csv_dict[prefix] = csv_dict[prefix][["Quintile", "Water Year"]]
+
+    # concat dfs
+    df = pd.concat(
+        [csv_dict["dry"], csv_dict["moderate"], csv_dict["wet"]], axis=0
+    )
+    # convert to dict
+    df.index = df["Water Year"]
+    df.drop(columns="Water Year", inplace=True)
+    dict_out = df["Quintile"].to_dict()
+    return dict_out
+
+
 # def load_netcdf_variable_definitions(instance, filename: str) -> dict:
 #     with open(filename, "r") as f:
 #         raw_definitions = yaml.safe_load(f)

4d4a985795a7fc4d1433517f13f012aaa9bc4ed0
Dillon Ragar <dillonragar@gmail.com>
2025-08-14 16:39:58 -0700

    update batch params

--- VegProcessor/batch_run.py
+++ VegProcessor/batch_run.py
@@ -5,15 +5,8 @@ from hsi import HSI
 
 veg_config_files = [
     "./configs/veg_config_1-08ft_slr_dry.yaml",
-    "./configs/veg_config_1-08ft_slr_moderate.yaml",
     "./configs/veg_config_1-08ft_slr_wet.yaml",
-    #
-    "./configs/veg_config_2ft_slr_dry.yaml",
-    "./configs/veg_config_2ft_slr_moderate.yaml",
-    "./configs/veg_config_2ft_slr_wet.yaml",
-    #
     "./configs/veg_config_base_dry.yaml",
-    "./configs/veg_config_base_moderate.yaml",
     "./configs/veg_config_base_wet.yaml",
 ]
 
@@ -21,33 +14,35 @@ veg_config_files = [
 # list of config files for each HSI run
 hsi_config_files = [
     "./configs/hsi_config_1-08ft_slr_dry.yaml",
-    "./configs/hsi_config_1-08ft_slr_moderate.yaml",
     "./configs/hsi_config_1-08ft_slr_wet.yaml",
-    #
-    "./configs/hsi_config_2ft_slr_dry.yaml",
-    "./configs/hsi_config_2ft_slr_moderate.yaml",
-    "./configs/hsi_config_2ft_slr_wet.yaml",
-    #
     "./configs/hsi_config_base_dry.yaml",
-    "./configs/hsi_config_base_moderate.yaml",
     "./configs/hsi_config_base_wet.yaml",
 ]
 
 
 def main():
-    # run each VegTransition scenario
-    for config in veg_config_files:
-        print(f"Running VegTransition model for config: {config}")
-        Veg = VegTransition(config_file=config)
-        Veg.run()
-        Veg.post_process()
+    run_veg = (
+        input("Do you want to run Veg models? (y/n): ").lower().strip() == "y"
+    )
+    run_hsi = (
+        input("Do you want to run HSI models? (y/n): ").lower().strip() == "y"
+    )
 
-    # run each HSI scenario
-    for config in hsi_config_files:
-        print(f"Running HSI model for config: {config}")
-        hsi = HSI(config_file=config)
-        hsi.run()
-        hsi.post_process()
+    if run_veg:
+        # run each VegTransition scenario
+        for config in veg_config_files:
+            print(f"Running VegTransition model for config: {config}")
+            veg = VegTransition(config_file=config)
+            veg.run()
+            veg.post_process()
+
+    if run_hsi:
+        # run each HSI scenario
+        for config in hsi_config_files:
+            print(f"Running HSI model for config: {config}")
+            hsi = HSI(config_file=config)
+            hsi.run()
+            hsi.post_process()
 
 
 if __name__ == "__main__":

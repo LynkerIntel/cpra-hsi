18dcaca7787c1fe77162328e5272fbb389fcce1c
Dillon Ragar <dillonragar@gmail.com>
2025-10-22 14:17:02 -0700

    update (need to make paths relative)

--- VegProcessor/batch_run.py
+++ VegProcessor/batch_run.py
@@ -20,15 +20,15 @@ veg_config_files = [
 # list of config files for each HSI run
 hsi_config_files = [
     # D3D
-    # "./configs/hsi_d3d_config_1-08ft_dry.yaml",
-    # "./configs/hsi_d3d_config_1-08ft_wet.yaml",
-    "./configs/hsi_d3d_config_base_dry.yaml",
-    "./configs/hsi_d3d_config_base_wet.yaml",
+    # "/Users/dillonragar/data/cpra/configs/hsi_d3d_config_1-08ft_dry.yaml",
+    # "/Users/dillonragar/data/cpra/configs/hsi_d3d_config_1-08ft_wet.yaml",
+    "/Users/dillonragar/data/cpra/configs/hsi_d3d_config_base_dry.yaml",
+    "/Users/dillonragar/data/cpra/configs/hsi_d3d_config_base_wet.yaml",
     # HEC
-    "./configs/hsi_hec_config_1-08ft_slr_dry.yaml",
-    "./configs/hsi_hec_config_1-08ft_slr_wet.yaml",
-    "./configs/hsi_hec_config_base_dry.yaml",
-    "./configs/hsi_hec_config_base_wet.yaml",
+    "/Users/dillonragar/data/cpra/configs/hsi_hec_config_1-08ft_slr_dry.yaml",
+    "/Users/dillonragar/data/cpra/configs/hsi_hec_config_1-08ft_slr_wet.yaml",
+    "/Users/dillonragar/data/cpra/configs/hsi_hec_config_base_dry.yaml",
+    "/Users/dillonragar/data/cpra/configs/hsi_hec_config_base_wet.yaml",
 ]
 
 

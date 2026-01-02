86b310d8c855726616561a963b1249f78518fa6e
Dillon Ragar <dillonragar@gmail.com>
2026-01-02 09:40:12 -0700

    run all

--- VegProcessor/batch_run.py
+++ VegProcessor/batch_run.py
@@ -31,8 +31,8 @@ hsi_config_files = [
     # D3D
     "/Users/dillonragar/data/cpra/configs/hsi_d3d_config_1-08ft_dry.yaml",
     "/Users/dillonragar/data/cpra/configs/hsi_d3d_config_1-08ft_wet.yaml",
-    # "/Users/dillonragar/data/cpra/configs/hsi_d3d_config_base_dry.yaml",
-    # "/Users/dillonragar/data/cpra/configs/hsi_d3d_config_base_wet.yaml",
+    "/Users/dillonragar/data/cpra/configs/hsi_d3d_config_base_dry.yaml",
+    "/Users/dillonragar/data/cpra/configs/hsi_d3d_config_base_wet.yaml",
     # HEC
     "/Users/dillonragar/data/cpra/configs/hsi_hec_config_1-08ft_slr_dry.yaml",
     "/Users/dillonragar/data/cpra/configs/hsi_hec_config_1-08ft_slr_wet.yaml",

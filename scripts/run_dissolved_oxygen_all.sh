f62bd7d8dc2f7c72433f91fe6a798bbc1e6dfc8c
Dillon Ragar <dillonragar@gmail.com>
2026-06-06 10:50:57 -0700

    bump oyxgen to v4

--- scripts/run_dissolved_oxygen_all.sh
+++ scripts/run_dissolved_oxygen_all.sh
@@ -27,63 +27,63 @@ run_one() {
 run_one "G400 WY06 SLR000" \
   --data-dir /Users/dillonragar/data/cpra \
   --group G400 --wy 06 --slr 000 \
-  --input-version V2 --output-version V3
+  --input-version V2 --output-version V4
 
 run_one "G400 WY06 SLR328" \
   --data-dir /Users/dillonragar/data/cpra \
   --group G400 --wy 06 --slr 328 \
-  --input-version V2 --output-version V3
+  --input-version V2 --output-version V4
 
 run_one "G400 WY20 SLR000" \
   --data-dir /Users/dillonragar/data/cpra \
   --group G400 --wy 20 --slr 000 \
-  --input-version V2 --output-version V3
+  --input-version V2 --output-version V4
 
 run_one "G400 WY20 SLR328" \
   --data-dir /Users/dillonragar/data/cpra \
   --group G400 --wy 20 --slr 328 \
-  --input-version V2 --output-version V3
+  --input-version V2 --output-version V4
 
 run_one "G400 WY22 SLR000" \
   --data-dir /Users/dillonragar/data/cpra \
   --group G400 --wy 22 --slr 000 \
-  --input-version V2 --output-version V3
+  --input-version V2 --output-version V4
 
 run_one "G400 WY22 SLR328" \
   --data-dir /Users/dillonragar/data/cpra \
   --group G400 --wy 22 --slr 328 \
-  --input-version V2 --output-version V3
+  --input-version V2 --output-version V4
 
 # --- G900 ---------------------------------------------------------------
 run_one "G900 WY06 SLR000" \
   --data-dir /Users/dillonragar/data/cpra \
   --group G900 --wy 06 --slr 000 \
-  --input-version V2 --output-version V3
+  --input-version V2 --output-version V4
 
 run_one "G900 WY06 SLR328" \
   --data-dir /Users/dillonragar/data/cpra \
   --group G900 --wy 06 --slr 328 \
-  --input-version V2 --output-version V3
+  --input-version V2 --output-version V4
 
 run_one "G900 WY20 SLR000" \
   --data-dir /Users/dillonragar/data/cpra \
   --group G900 --wy 20 --slr 000 \
-  --input-version V2 --output-version V3
+  --input-version V2 --output-version V4
 
 run_one "G900 WY20 SLR328" \
   --data-dir /Users/dillonragar/data/cpra \
   --group G900 --wy 20 --slr 328 \
-  --input-version V2 --output-version V3
+  --input-version V2 --output-version V4
 
 run_one "G900 WY22 SLR000" \
   --data-dir /Users/dillonragar/data/cpra \
   --group G900 --wy 22 --slr 000 \
-  --input-version V2 --output-version V3
+  --input-version V2 --output-version V4
 
 run_one "G900 WY22 SLR328" \
   --data-dir /Users/dillonragar/data/cpra \
   --group G900 --wy 22 --slr 328 \
-  --input-version V2 --output-version V3
+  --input-version V2 --output-version V4
 
 # --- Summary ------------------------------------------------------------
 echo

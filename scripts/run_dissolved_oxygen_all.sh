6657213e3fa14a76d66f43224d7fec497ade0f79
Dillon Ragar <dillonragar@gmail.com>
2026-07-15 10:06:41 -0700

    G401

--- scripts/run_dissolved_oxygen_all.sh
+++ scripts/run_dissolved_oxygen_all.sh
@@ -26,32 +26,32 @@ run_one() {
 
 # ------ G401 -----------------------------------------------------------
 run_one "G401 WY06 SLR000" \
-  --data-dir /Users/dillonragar/data/cpra \
+  --data-dir '/Volumes/Extreme SSD/cpra/G401_zarr' \
   --group G401 --wy 06 --slr 000 \
   --input-version V2 --output-version V4
 
 run_one "G401 WY06 SLR328" \
-  --data-dir /Users/dillonragar/data/cpra \
+  --data-dir '/Volumes/Extreme SSD/cpra/G401_zarr' \
   --group G401 --wy 06 --slr 328 \
   --input-version V2 --output-version V4
 
 run_one "G401 WY20 SLR000" \
-  --data-dir /Users/dillonragar/data/cpra \
+  --data-dir '/Volumes/Extreme SSD/cpra/G401_zarr' \
   --group G401 --wy 20 --slr 000 \
   --input-version V2 --output-version V4
 
 run_one "G401 WY20 SLR328" \
-  --data-dir /Users/dillonragar/data/cpra \
+  --data-dir '/Volumes/Extreme SSD/cpra/G401_zarr' \
   --group G401 --wy 20 --slr 328 \
   --input-version V2 --output-version V4
 
 run_one "G401 WY22 SLR000" \
-  --data-dir /Users/dillonragar/data/cpra \
+  --data-dir '/Volumes/Extreme SSD/cpra/G401_zarr' \
   --group G401 --wy 22 --slr 000 \
   --input-version V2 --output-version V4
 
 run_one "G401 WY22 SLR328" \
-  --data-dir /Users/dillonragar/data/cpra \
+  --data-dir '/Volumes/Extreme SSD/cpra/G401_zarr' \
   --group G401 --wy 22 --slr 328 \
   --input-version V2 --output-version V4
 
